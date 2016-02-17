a0156202601de3326a67099a282f5144bb07b4fc
st7michael <st7michael@163.com>
2016-02-17 16:01:37 +0800

    Initial Commit

---
+++ GAME!/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GAME!
+//
+//  Created by Yiqiao on 16/2/17.
+//  Copyright © 2016年 Yiqiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GAME!/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GAME!
+//
+//  Created by Yiqiao on 16/2/17.
+//  Copyright © 2016年 Yiqiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
