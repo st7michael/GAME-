89e374d5c42baf2adb28bc4a73b2dc741b9320d0
st7michael <st7michael@163.com>
2016-02-17 17:59:55 +0800

    base 01

---
+++ GAME!/gameViewController.h
@@ -0,0 +1,13 @@
+//
+//  gameViewController.h
+//  GAME!
+//
+//  Created by Yiqiao on 16/2/17.
+//  Copyright © 2016年 Yiqiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface gameViewController : UIViewController
+
+@end
---
+++ GAME!/introViewController.h
@@ -0,0 +1,13 @@
+//
+//  introViewController.h
+//  GAME!
+//
+//  Created by Yiqiao on 16/2/17.
+//  Copyright © 2016年 Yiqiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface introViewController : UIViewController
+
+@end
